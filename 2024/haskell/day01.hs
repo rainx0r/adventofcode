30905eb4182cc2961fe75a87d98f174319da9d32
rainx0r <angelos.h@hotmail.com>
2024-12-01 16:41:36 +0000

    Update Day 1 Haskell solution to remove double Where

--- 2024/haskell/day01.hs
+++ 2024/haskell/day01.hs
@@ -3,16 +3,14 @@ import Data.List
 toTuple :: [a] -> (a, a)
 toTuple [x, y] = (x, y)
 
-
 day1part1 :: IO Integer
 day1part1 = sum . map abs . map (foldr (-) 0) . transpose . (map sort) . transpose . map (map read) . (map words) . lines <$> readFile "data/day01.txt"
 
 day1part2 :: IO Integer
 day1part2 = solve . toTuple . transpose . map (map read) . (map words) . lines <$> readFile "data/day01.txt"
- where
-  solve (x1, x2) = sum (zipWith (*) x1 (map (getCountSingle x2) x1))
-    where
-      getCountSingle yy x = (toInteger . length) (filter (== x) yy)
+  where
+    solve (x1, x2) = sum (zipWith (*) x1 (map (getCountSingle x2) x1))
+    getCountSingle yy x = (toInteger . length) (filter (== x) yy)
 
 main :: IO ()
 main = do
