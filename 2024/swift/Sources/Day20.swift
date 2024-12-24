4936df586aa006508e9d1cc974ce8886de625da6
rainx0r <angelos.h@hotmail.com>
2024-12-24 13:13:41 +0200

    Solve Day 20 in Swift

--- 2024/swift/Sources/Day20.swift
+++ 2024/swift/Sources/Day20.swift
@@ -1,9 +1,6 @@
-import Algorithms
-
-
 struct Day20: AdventDay {
     var data: String
-    
+
     struct Coord: Hashable {
         let x: Int
         let y: Int
@@ -12,12 +9,21 @@ struct Day20: AdventDay {
             self.x = x
             self.y = y
         }
-        
+
         static func + (lhs: Coord, rhs: Coord) -> Coord {
             return Coord(lhs.x + rhs.x, lhs.y + rhs.y)
         }
+
+        func manhattan(_ other: Coord) -> Int {
+            return abs(x - other.x) + abs(y - other.y)
+        }
     }
     
+    struct CoordPair: Hashable {
+        let coord1: Coord
+        let coord2: Coord
+    }
+
     enum Direction: Character, CaseIterable {
         case north = "^"
         case south = "v"
@@ -45,14 +51,69 @@ struct Day20: AdventDay {
 
     struct Racetrack {
         let map: [Coord: Character]
+        let bounds: Coord
         let startPos: Coord
         let endPos: Coord
+
+        func getTrack() -> (track: [Coord], coordIdx: [Coord: Int]) {
+            var _track: [Coord] = [startPos]
+            var pos = 0
+            var coordIdx: [Coord: Int] = [startPos: pos]
+            var current = startPos
+            var previous: Coord? = nil
+            while current != endPos {
+                var next: Coord? = nil
+                for direction in Direction.allCases {
+                    let nextCoord = current + direction.coord()
+                    if nextCoord != previous && map[nextCoord] != nil && map[nextCoord] != "#" {
+                        next = current + direction.coord()
+                        break
+                    }
+                }
+                if next != nil {
+                    _track.append(next!)
+                    previous = current
+                    current = next!
+                    pos += 1
+                    coordIdx[next!] = pos
+                }
+            }
+            return (_track, coordIdx)
+        }
+
+        func getNumCheats(track: (track: [Coord], coordIdx: [Coord: Int]), cheatDist: Int, saveAtLeast: Int) -> Int {
+            var visited = Set<CoordPair>()
+            var count = 0
+            for (time1, coord1) in track.track.enumerated() {
+                for yOffset in -cheatDist...cheatDist {
+                    if !(coord1.y + yOffset >= 0 && coord1.y + yOffset < bounds.y) {
+                       continue
+                    }
+                    let xBound = cheatDist - abs(yOffset)
+                    for xOffset in -xBound...xBound {
+                        if xOffset == 0 && yOffset == 0 { continue }
+                        if !(coord1.x + xOffset >= 0 && coord1.x + xOffset < bounds.y) {
+                           continue
+                        }
+                        let coord2 = coord1 + Coord(xOffset, yOffset)
+                        if map[coord2] != "#" && !visited.contains(CoordPair(coord1: coord1, coord2: coord2)) {
+                            visited.insert(CoordPair(coord1: coord1, coord2: coord2))
+                            if track.coordIdx[coord2]! - time1 - coord2.manhattan(coord1) >= saveAtLeast {
+                                count += 1
+                            }
+                        }
+                    }
+                }
+            }
+            return count
+        }
     }
 
-    var entities: Racetrack {
-        let inputLines = self.data.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: "\n")
+    var entities: (rt: Racetrack, track: ([Coord], [Coord: Int])) {
+        let inputLines = data.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: "\n")
         var map: [Coord: Character] = [:], startPos: Coord? = nil, endPos: Coord? = nil
         map.reserveCapacity(inputLines.count * inputLines.first!.count)
+        let bounds = Coord(inputLines.first!.count, inputLines.count)
         for (y, line) in inputLines.enumerated() {
             xIter: for (x, char) in line.enumerated() {
                 let coord = Coord(x, y)
@@ -67,14 +128,15 @@ struct Day20: AdventDay {
                 }
             }
         }
-        return Racetrack(map: map, startPos: startPos!, endPos: endPos!)
+        let rt = Racetrack(map: map, bounds: bounds, startPos: startPos!, endPos: endPos!)
+        return (rt, rt.getTrack())
     }
 
     func part1() -> Any {
-        0
+        return entities.rt.getNumCheats(track: entities.track, cheatDist: 2, saveAtLeast: 100)
     }
 
     func part2() -> Any {
-        0
+        return entities.rt.getNumCheats(track: entities.track, cheatDist: 20, saveAtLeast: 100)
     }
 }
